e51014c2096f724dc68da3c95aa468348534a586
Nina Yeh <ninayeh1211@gmail.com>
2019-10-09 20:52:48 +0100

    fetch movie api

---
+++ src/components/elements/Actor/Actor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {IMAGE_BASE_URL, POST_SIZE, BACKDROP_SIZE} from '../../../config';
+import './Actor.css';
+
+const Actor = (props) => {
+  return(
+    <div>Actor</div>
+  )
+}
+
+export default Actor;
