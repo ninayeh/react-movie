a561fb3cad95374e5a0f5adb805097aace762a77
Nina Yeh <ninayeh1211@gmail.com>
2019-10-13 18:31:00 +0100

    deploy test

---
+++ build/precache-manifest.62978a7a4b1b54e32d15fc488c52f7d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04291a56cd6e525d0e5534c6344cc390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4242ba14d60610c3bd",
+    "url": "/static/css/main.e2bb231f.chunk.css"
+  },
+  {
+    "revision": "06d4aaf5d865b8d1f51d",
+    "url": "/static/js/2.687255bd.chunk.js"
+  },
+  {
+    "revision": "af4242ba14d60610c3bd",
+    "url": "/static/js/main.dac5d7fb.chunk.js"
+  },
+  {
+    "revision": "5e2bbe5c72ac3dcf6bd1",
+    "url": "/static/js/runtime-main.b106d53e.js"
+  }
+]);
