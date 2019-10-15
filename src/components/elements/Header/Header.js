16d9ead133e3d791843b21dac60bddd563b04792
Nina Yeh <ninayeh1211@gmail.com>
2019-10-15 14:19:05 +0100

    fix image path

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -5,13 +5,12 @@ import './Header.css';
 
 const Header = () => {
   return (
-    // React.createElement('div', {className: 'react_div'}, "This is a test");
     <div className="rmdb-header">
       <div className="rmdb-content">
         <Link to='/'>
-          <img className="rmdb-logo" src="./Images/reactMovie_logo.png" alt="rmbd-logo" />
+          <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmbd-logo" />
         </Link>
-        <img className="rmdb-tmdb-logo" src="./Images/tmdb_logo.png" alt="tmbd-logo" />
+        <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmbd-logo" />
       </div>
     </div>
 
