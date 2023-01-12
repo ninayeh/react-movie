b3816225fa49a2c2ec9806882b5417009b172c36
ninayeh <itsmeninayeh@gmail.com>
2023-01-12 11:02:33 +0900

    Update API URL from my own server to themoviedb.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 // Use ruby on rails server to fetch TMDB
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
-const API_URL = 'https://read-movie-api.herokuapp.com/v1/';
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+
 
 // Images
 const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
@@ -13,6 +16,7 @@ const POSTER_SIZE = 'w500'
 
 export {
   API_URL,
+  API_KEY,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
   POSTER_SIZE
