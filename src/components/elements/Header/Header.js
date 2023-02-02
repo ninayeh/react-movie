98a1a166fdfbc33b7ac5946b54a8abadc614dbe4
ninayeh <itsninayeh@gmail.com>
2023-02-02 23:02:34 +0900

    Update header style and TMDB logo

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -10,7 +10,8 @@ const Header = () => {
         <Link to='/'>
           <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmbd-logo" />
         </Link>
-        <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmbd-logo" />
+
+        <img className="rmdb-tmdb-logo" src="/images/tmdb_logo_new.png" alt="tmbd-logo" />
       </div>
     </div>
 
