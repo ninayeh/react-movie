aff49bbc0ee8c98ba8a657843166cc170d061e17
ninayeh <itsninayeh@gmail.com>
2023-02-03 16:44:18 +0900

    Fix some issue in Navigation elements

--- src/components/elements/Navigation/Navigation.js
+++ src/components/elements/Navigation/Navigation.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navigation.css'
 
 const Navigation = (props) => {
-
-  return(
+  return (
     <div className="rmdb-navigation">
       <div className="rmdb-navigation-content">
         <Link to='/'>
           <p>Home</p>
         </Link>
         <p>/</p>
-        <p>{props.movie}</p>
+        <p>{props.movie.original_title}</p>
       </div>
     </div>
   )
